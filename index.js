628b9cbcc28c782ee9ba65e883bccc4a89bf6839
Crazy-Candyman <124856808+Crazy-Candyman@users.noreply.github.com>
2023-04-22 16:29:28 -0400

    New Pitcher Button
    added new pitcher button to reset the count.
    saved pitch count to local storage so that if it refreshes it does not erase information (count)

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
+let homeCountEl = document.getElementById("home-count-el");
 let count = 0;
+let storedHomePitches = JSON.parse(localStorage.getItem("homepitches"));
+count += storedHomePitches;
+homeCountEl.textContent = storedHomePitches;
 
 function increment() {
   count += 1;
-  countEl.textContent = count;
+  homeCountEl.textContent = count;
+  localStorage.setItem("homepitches", JSON.stringify(count));
 }
 
-function save() {
-  let countStr = " " + count + " - ";
-  saveEl.textContent += countStr;
-  countEl.textContent = 0;
+function decrement() {
+  count -= 1;
+  homeCountEl.textContent = count;
+  localStorage.setItem("homepitches", JSON.stringify(count));
+}
+
+function reset() {
+  homeCountEl.textContent = 0;
   count = 0;
+  localStorage.clear("homepitches");
 }
