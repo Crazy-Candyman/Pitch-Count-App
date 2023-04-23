9b967576a85630ff84aeddf10f9cb88e7c0160fd
Crazy-Candyman <124856808+Crazy-Candyman@users.noreply.github.com>
2023-04-23 18:04:01 -0400

    Major Update
    local storage save for pitch counter, added home and away pitchers and link to both sides. new font style and decorations

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
-let homeCountEl = document.getElementById("home-count-el");
+let countElHome = document.getElementById("count-elHome");
+let countElAway = document.getElementById("count-elAway");
 let count = 0;
-let storedHomePitches = JSON.parse(localStorage.getItem("homepitches"));
-count += storedHomePitches;
-homeCountEl.textContent = storedHomePitches;
+let storedPitches = JSON.parse(localStorage.getItem("homepitches"));
+let awayPitches = JSON.parse(localStorage.getItem("awaypitches"));
 
-function increment() {
+if (window.location.pathname === "/index.html") {
+  countElHome.textContent = storedPitches;
+  count += storedPitches;
+  if (countElHome.textContent === "") {
+    countElHome.textContent = 0;
+  }
+}
+
+if (window.location.pathname === "/pagetwo.html") {
+  countElAway.textContent = awayPitches;
+  count += awayPitches;
+  if (countElAway.textContent === "") {
+    countElAway.textContent = 0;
+  }
+}
+
+function incrementHome() {
   count += 1;
-  homeCountEl.textContent = count;
+  countElHome.textContent = count;
   localStorage.setItem("homepitches", JSON.stringify(count));
 }
 
-function decrement() {
+function decrementHome() {
   count -= 1;
-  homeCountEl.textContent = count;
+  countElHome.textContent = count;
   localStorage.setItem("homepitches", JSON.stringify(count));
 }
 
-function reset() {
-  homeCountEl.textContent = 0;
+function resetHome() {
+  countElHome.textContent = 0;
   count = 0;
-  localStorage.clear("homepitches");
+  localStorage.removeItem("homepitches");
+}
+
+//AWAY TEAM
+
+function incrementAway() {
+  count += 1;
+  countElAway.textContent = count;
+  localStorage.setItem("awaypitches", JSON.stringify(count));
+}
+
+function decrementAway() {
+  count -= 1;
+  countElAway.textContent = count;
+  localStorage.setItem("awaypitches", JSON.stringify(count));
+}
+
+function resetAway() {
+  countElAway.textContent = 0;
+  count = 0;
+  localStorage.removeItem("awaypitches");
 }
