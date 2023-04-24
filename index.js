5801d5b7184717b1c4d64be995616daff34341db
Crazy-Candyman <124856808+Crazy-Candyman@users.noreply.github.com>
2023-04-24 11:24:35 -0400

    Update JS file
    switched to session storage.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let countElHome = document.getElementById("count-elHome");
 let countElAway = document.getElementById("count-elAway");
 let count = 0;
-let storedPitches = JSON.parse(localStorage.getItem("homepitches"));
-let awayPitches = JSON.parse(localStorage.getItem("awaypitches"));
+let storedPitches = JSON.parse(sessionStorage.getItem("homepitches"));
+let awayPitches = JSON.parse(sessionStorage.getItem("awaypitches"));
 
 if (window.location.pathname === "/index.html") {
   count += storedPitches;
@@ -13,29 +13,29 @@ if (window.location.pathname === "/index.html") {
 }
 
 if (window.location.pathname === "/pagetwo.html") {
-  countElAway.textContent = awayPitches;
   count += awayPitches;
-  if (countElAway.textContent === "") {
-    countElAway.textContent = 0;
-  }
+  countElAway.textContent = count;
+  // if (countElAway.textContent === "") {
+  //   countElAway.textContent = 0;
+  // }
 }
 
 function incrementHome() {
   count += 1;
   countElHome.textContent = count;
-  localStorage.setItem("homepitches", JSON.stringify(count));
+  sessionStorage.setItem("homepitches", JSON.stringify(count));
 }
 
 function decrementHome() {
   count -= 1;
   countElHome.textContent = count;
-  localStorage.setItem("homepitches", JSON.stringify(count));
+  sessionStorage.setItem("homepitches", JSON.stringify(count));
 }
 
 function resetHome() {
   countElHome.textContent = 0;
   count = 0;
-  localStorage.removeItem("homepitches");
+  sessionStorage.removeItem("homepitches");
 }
 
 //AWAY TEAM
@@ -43,17 +43,17 @@ function resetHome() {
 function incrementAway() {
   count += 1;
   countElAway.textContent = count;
-  localStorage.setItem("awaypitches", JSON.stringify(count));
+  sessionStorage.setItem("awaypitches", JSON.stringify(count));
 }
 
 function decrementAway() {
   count -= 1;
   countElAway.textContent = count;
-  localStorage.setItem("awaypitches", JSON.stringify(count));
+  sessionStorage.setItem("awaypitches", JSON.stringify(count));
 }
 
 function resetAway() {
   countElAway.textContent = 0;
   count = 0;
-  localStorage.removeItem("awaypitches");
+  sessionStorage.removeItem("awaypitches");
 }
