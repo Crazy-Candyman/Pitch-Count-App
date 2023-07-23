81bb9902e30aea51144c7eaae00cdaf5a32fcfbe
Crazy-Candyman <gabenina1@gmail.com>
2023-07-22 21:36:17 -0400

    update to page two

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // Listen for install of the service worker event
-self.addEventListener("install", () => {
+self.addEventListener('install', () => {
   console.log("Service worker successfully installed");
 });
 
 // Listen for activate of the service worker event
-self.addEventListener("activate", () => {
+self.addEventListener('activate', () => {
   console.log("Service worker successfully activated");
 });
 
 // Fetch events
-self.addEventListener("fetch", (fetch) => {
+self.addEventListener('fetch', (fetch) => {
   console.log("Fetch event", fetch);
 });
